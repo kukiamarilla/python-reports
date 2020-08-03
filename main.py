2fb8b3bc4feeddf96d9bb4eee44c16ec0b005630
Kuki Amarilla <sc.amarilla@gmail.com>
2020-08-03 01:04:17 -0400

    Commit Inicial

---
+++ main.py
@@ -0,0 +1,18 @@
+import jinja2
+import pandas as pd
+import pdfkit
+
+data = pd.read_csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/owid-covid-data.csv")
+data = data[data["location"]=="Paraguay"]
+data = data.dropna()
+dataset = data[["total_cases","date"]]
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "report.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+outputText = template.render(df=dataset)
+html_file = open('./output/out.html', 'w')
+html_file.write(outputText)
+html_file.close()
+pdfkit.from_file("./output/out.html","./output/out.pdf")
